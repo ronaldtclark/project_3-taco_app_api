829a1457670ac3841e9e672fe005a66ed92421cf
Ron Clark <ronaldtclark@gmail.com>
2018-08-10 10:35:34 -0500

    changed db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/tacos'
 
-mongoose.connect('mongodb://localhost/tacos')
+mongoose.connect(mongoUri, {useNewUrlParser: true})
 
 mongoose.connection.on('connected', () => {
   console.log('mongoose is connected')
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const session = require('express-session')
 
+const PORT = process.env.PORT || 9000;
+
 require('./db/db')
+
 app.use(session({
   secret: 'secret',
   resave: false,
@@ -27,6 +30,6 @@ const authController = require('./controllers/authController')
 app.use('/tacos', tacoController)
 app.use('/auth/login', authController)
 
-app.listen(9000, () => {
-  console.log('TACO APP API listening on port 9000')
+app.listen(PORT, () => {
+  console.log('TACO APP API listening on port', PORT)
 })
